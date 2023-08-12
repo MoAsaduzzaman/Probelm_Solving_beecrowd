b481c2aa61961f292e1ac0b8014fbec7137e7d5f
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-13 00:20:59 +0600

    1002

---
+++ 1002.c
@@ -0,0 +1,14 @@
+//Problem no-1002
+//https://www.beecrowd.com.br/judge/en/problems/view/1002
+
+#include<stdio.h>
+int main(){
+
+    double A,R;
+    scanf("%lf",&R);
+    A = 3.14159 * (R * R);
+
+    printf("A=%.4lf\n", A);
+
+    return 0;
+}
