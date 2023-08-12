aeab4660b1138448dc253d77236e6c47ddd614ff
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-13 00:30:15 +0600

    1003

---
+++ 1003.c
@@ -0,0 +1,13 @@
+//Problem no-1003
+//https://www.beecrowd.com.br/judge/en/problems/view/1003
+
+#include<stdio.h>
+int main(){
+
+    int A,B,SOMA;
+    scanf("%d %d ",&A, &B);
+    SOMA = A + B;
+
+    printf("SOMA = %d\n", SOMA);
+    return 0;
+}
