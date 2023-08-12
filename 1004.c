dd250e266efcd5734504fab16f435409319ded6b
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-13 00:39:34 +0600

    1004

---
+++ 1004.c
@@ -0,0 +1,13 @@
+//Problem no-1004
+//https://www.beecrowd.com.br/judge/en/problems/view/1004
+
+#include<stdio.h>
+int main(){
+
+    int a,b;
+    scanf("%d %d ",&a,&b);
+    int PROD = a * b ;
+
+    printf("PROD = %d\n", PROD);
+    return 0;
+}
