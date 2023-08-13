d4110b8f48577bc557ed7e516b596da18ac7eb1e
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-13 18:20:24 +0600

    Distance

---
+++ 1016.c
@@ -0,0 +1,13 @@
+//Problem no-1016
+//https://www.beecrowd.com.br/judge/en/problems/view/1016
+
+#include<stdio.h>
+int main(){
+    int km;
+    scanf("%d",&km);
+
+    int min= (km*2);
+
+    printf("%d minutos\n",min);
+    return 0;
+}
