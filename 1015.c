ac5e49a3021b482d3f1244c5cd8734535ac7d0cd
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-13 18:09:40 +0600

    Distance between two points

---
+++ 1015.c
@@ -0,0 +1,17 @@
+//Problem no-1015
+//https://www.beecrowd.com.br/judge/en/problems/view/1015
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+    double x1,x2,y1,y2,dis;
+    scanf("%lf %lf", &x1, &y1);
+    scanf("%lf %lf", &x2, &y2);
+
+    dis =sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+    
+    printf("%.4lf\n",dis);
+
+    return 0;
+}
