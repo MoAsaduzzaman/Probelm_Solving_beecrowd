31c38285969d7817e1d67854f17068d2e44d0f4b
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-15 00:09:00 +0600

    Snack

---
+++ 1038.c
@@ -0,0 +1,27 @@
+//Problem no-1038
+//https://www.beecrowd.com.br/judge/en/problems/view/1038
+
+#include<stdio.h>
+int main(){
+    int X,Y;
+    double price=0;
+    scanf("%d %d",&X ,&Y);
+
+    if(X==1){
+        printf("Total: R$ %.2lf\n",price=Y*4.00);
+    }
+    else if(X==2){
+        printf("Total: R$ %.2lf\n",price=Y*4.50);
+    }
+    else if(X==3){
+        printf("Total: R$ %.2lf\n",price=Y*5.00);
+    }
+    else if(X==4){
+        printf("Total: R$ %.2lf\n",price=Y*2.00);
+    }
+    else if(X==5){
+        printf("Total: R$ %.2lf\n",price=Y*1.50);
+    }
+
+    return 0;
+}
