3a4cbaee0b880b494934e5ecf766be9d44d848c9
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-15 20:18:51 +0600

    Multiples

---
+++ 1044.c
@@ -0,0 +1,17 @@
+//Problem no-1044
+//https://www.beecrowd.com.br/judge/en/problems/view/1044
+
+#include<stdio.h>
+int main(){
+    int A,B;
+    scanf("%d %d",&A, &B);
+
+    if(B%A==0 || A%B==0){
+        printf("Sao Multiplos\n");
+    }
+    else{
+        printf("Nao sao Multiplos\n");
+    }
+
+    return 0;
+}
