49a1175aa3a9811f1812e8a3d98c033f0fba4b56
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-16 10:54:00 +0600

    Even Numbers

---
+++ 1059.c
@@ -0,0 +1,13 @@
+//Problem no-1059
+//https://www.beecrowd.com.br/judge/en/problems/view/1059
+
+#include<stdio.h>
+int main(){
+    for(int i=2 ; i<=100 ;i++){
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+    }
+
+    return 0;
+}
