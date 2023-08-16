822a94f3dbdcdf914f3b28171bf99f11363a5aea
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-16 22:41:02 +0600

    Remaining 2

---
+++ 1075.c
@@ -0,0 +1,16 @@
+//Problem no-1075
+//https://www.beecrowd.com.br/judge/en/problems/view/1075
+
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+
+    for(int i=1; i<10000; i++){
+        if(i%N==2){
+            printf("%d\n",i);
+        }
+    }
+    
+    return 0;
+}
