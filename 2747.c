48ac9ea1d4f7b50ff9d33d079235a549a8a70088
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-19 11:35:56 +0600

    Output 1

---
+++ 2747.c
@@ -0,0 +1,16 @@
+//Problem no-2747
+//https://www.beecrowd.com.br/judge/en/problems/view/2747
+
+#include<stdio.h>
+int main(){
+
+    printf("---------------------------------------\n");
+    printf("|                                     |\n");
+    printf("|                                     |\n");
+    printf("|                                     |\n");
+    printf("|                                     |\n");
+    printf("|                                     |\n");
+    printf("---------------------------------------\n");
+
+    return 0;
+}
