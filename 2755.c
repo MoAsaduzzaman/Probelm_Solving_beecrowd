0834655e8068aa597ee70853aef383cde99d7077
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-19 11:03:39 +0600

    Output 9

---
+++ 2755.c
@@ -0,0 +1,13 @@
+//Problem no-2755
+//https://www.beecrowd.com.br/judge/en/problems/view/2755
+
+#include<stdio.h>
+int main(){
+
+    printf("\"Ro'b'er\tto\\/\"\n");
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") ('.')\n");
+
+    return 0;
+}
