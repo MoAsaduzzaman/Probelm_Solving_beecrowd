bea5af26110f834406d5366918f324d45a2058a7
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-20 10:23:10 +0600

    Output 3

---
+++ 2749.c
@@ -0,0 +1,16 @@
+//Problem no-2749
+//https://www.beecrowd.com.br/judge/en/problems/view/2749
+
+#include<stdio.h>
+int main(){
+
+    printf("---------------------------------------\n");
+    printf("|x = 35                               |\n");
+    printf("|                                     |\n");
+    printf("|               x = 35                |\n");
+    printf("|                                     |\n");
+    printf("|                               x = 35|\n");
+    printf("---------------------------------------\n");
+
+    return 0;
+}
