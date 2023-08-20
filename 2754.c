8f0607964670be069a6a060e0a0a147d237c1500
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-20 10:26:08 +0600

    Output 8

---
+++ 2754.c
@@ -0,0 +1,20 @@
+//Problem no-2754
+//https://www.beecrowd.com.br/judge/en/problems/view/2754
+
+#include<stdio.h>
+int main(){
+    double x=234.345;
+    double y=45.698;
+
+    printf("234.345000 - 45.698000\n");
+    printf("234 - 46\n");
+    printf("234.3 - 45.7\n");
+    printf("234.34 - 45.70\n");
+    printf("234.345 - 45.698\n");
+    printf("2.343450e+02 - 4.569800e+01\n");
+    printf("2.343450E+02 - 4.569800E+01\n");
+    printf("234.345 - 45.698\n");
+    printf("234.345 - 45.698\n");
+
+    return 0;
+}
