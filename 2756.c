cd86456f6e1ab779caaa5188ef39f01762a84b36
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-21 07:34:15 +0600

    Output 10

---
+++ 2756.c
@@ -0,0 +1,18 @@
+//Problem no-2756
+//https://www.beecrowd.com.br/judge/en/problems/view/2756
+
+#include<stdio.h>
+int main(){
+    
+    printf("       A\n");
+    printf("      B B\n");
+    printf("     C   C\n");
+    printf("    D     D\n");
+    printf("   E       E\n");
+    printf("    D     D\n");
+    printf("     C   C\n");
+    printf("      B B\n");
+    printf("       A\n");
+
+    return 0;
+}
