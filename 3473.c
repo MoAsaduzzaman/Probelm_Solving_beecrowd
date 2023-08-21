2ab83043bd66c97e2674c77a4f311569e29e11d9
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-21 07:38:29 +0600

    Alert the next one!

---
+++ 3473.c
@@ -0,0 +1,14 @@
+//Problem no-3473
+//https://www.beecrowd.com.br/judge/en/problems/view/3473
+
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    
+    N+=1;
+    printf("%d\n",N);
+
+    return 0;
+}
+
