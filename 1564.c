45eaa43f39ba9d6cc9ca0f1ce57fb527983877cf
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-21 07:31:39 +0600

    Brazil World Cup

---
+++ 1564.c
@@ -0,0 +1,17 @@
+//Problem no-1564
+//https://www.beecrowd.com.br/judge/en/problems/view/1564
+
+#include<stdio.h>
+int main(){
+    int N;
+    while(scanf("%d",&N)!=EOF){       
+        if(N==0){
+            printf("vai ter copa!\n");
+            
+        }else{
+            printf("vai ter duas!\n");
+        }       
+    }
+
+    return 0;
+}
