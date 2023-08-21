2b7ad6f1041a87928f9bc748e0ec246cff793538
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-21 07:33:24 +0600

    Ho Ho Ho

---
+++ 1759.c
@@ -0,0 +1,15 @@
+//Problem no-1759
+//https://www.beecrowd.com.br/judge/en/problems/view/1759
+
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+
+    for(int i=1; i<N; i++){
+        printf("Ho ");
+    }
+    printf("Ho!\n");
+
+    return 0;
+}
