2f68e3c357a8697a3e2c6c461047f0c3ea485fa5
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-22 18:33:40 +0600

    Identifying Tea

---
+++ 2006.c
@@ -0,0 +1,19 @@
+//Problem no-2006
+//https://www.beecrowd.com.br/judge/en/problems/view/2006
+
+//Way-1
+#include<stdio.h>
+int main(){
+    int T,arr[5],i,sum=0;
+    scanf("%d",&T);
+   
+    for(i=0; i<5; i++){
+        scanf("%d",&arr[i]);
+        if(arr[i]==T){
+            sum++;
+        }
+    }
+    printf("%d\n",sum);
+
+    return 0;
+}
