8d5ac2930201951485e71ee96176d27e3b0fa197
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-22 18:43:46 +0600

    Busca na Internet

---
+++ 2413.c
@@ -0,0 +1,13 @@
+//Problem no-2006
+//https://www.beecrowd.com.br/judge/en/problems/view/2006
+
+#include <stdio.h> 
+int main() {
+    int a,total;
+    scanf("%d",&a);
+    
+    total=a*4;
+    printf("%d\n",total);
+ 
+    return 0;
+}
