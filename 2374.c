77530e3a5ecf2e42446fa64c8cec36152015f7c7
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-22 18:50:55 +0600

    Pneu

---
+++ 2374.c
@@ -0,0 +1,15 @@
+//Problem no-2374
+//https://www.beecrowd.com.br/judge/en/problems/view/2374
+
+#include<stdio.h>
+int main(){
+    int a,b,sub;
+    scanf("%d %d",&a,&b);
+
+    sub=a-b;
+
+    printf("%d\n",sub);
+
+    return 0;
+}
+
