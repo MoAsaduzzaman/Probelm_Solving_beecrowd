f6079a0cf9a4ba951daa1c82840a413e7dbc05cb
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-22 08:13:04 +0600

    Our Days Are Never Comming Back

---
+++ 1864.c
@@ -0,0 +1,17 @@
+//Problem no-1864
+//https://www.beecrowd.com.br/judge/en/problems/view/1864
+
+#include<stdio.h>
+int main(){
+
+    int x;
+    char st[34]={"LIFE IS NOT A PROBLEM TO BE SOLVED"};
+    scanf("%d",&x);
+        for(int i=0;i<x;i++){
+        printf("%c",st[i]);
+        }
+        printf("\n");
+    
+    return 0;
+}
+
