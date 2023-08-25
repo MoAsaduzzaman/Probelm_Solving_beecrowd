8ca34a7a949e570ec2c641d430bcb8d08ab155f3
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-26 00:14:03 +0600

    Password

---
+++ 2146.c
@@ -0,0 +1,14 @@
+//Problem no-2146
+//https://www.beecrowd.com.br/judge/en/problems/view/2146
+
+#include<stdio.h>
+int main(){
+    int N,x;
+
+    while(scanf("%d",&N)!=EOF){
+    x=N-1;
+    printf("%d\n",x);
+    }
+
+    return 0;
+}
