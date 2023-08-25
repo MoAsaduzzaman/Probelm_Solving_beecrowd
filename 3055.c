f83526ac24ccdfa111f1bc9cd95b38290a0046be
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-25 09:02:59 +0600

    Nota Esquecida

---
+++ 3055.c
@@ -0,0 +1,15 @@
+//Problem no-3055
+//https://www.beecrowd.com.br/judge/en/problems/view/3055
+
+#include<stdio.h>
+int main(){
+
+    int A,M,x;
+    scanf("%d %d",&A,&M);
+
+    x=(M*2)-A;
+
+    printf("%d\n",x);
+
+    return 0;
+}
