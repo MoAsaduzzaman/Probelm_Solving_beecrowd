1b09d6e96f3c9ad0ca5550c7ce5cb739b79bf8eb
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-26 00:42:20 +0600

    Approximate Number of Primes

---
+++ 2159.c
@@ -0,0 +1,16 @@
+//Problem no-2159
+//https://www.beecrowd.com.br/judge/en/problems/view/2159
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    double n,ans1,ans2;
+    scanf("%lf",&n);
+
+    ans1=n/log(n);
+    ans2=1.25506*(n/log(n));
+
+    printf("%.1lf %.1lf\n",ans1,ans2);
+
+    return 0;
+}
