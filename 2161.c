fafd13eb94107445eda4c7b9ef6ef999e0fae1a4
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-26 01:02:56 +0600

    Square Root of 2

---
+++ 2161.c
@@ -0,0 +1,16 @@
+//Problem no-2161
+//https://www.beecrowd.com.br/judge/en/problems/view/2161
+
+#include<stdio.h>
+int main(){
+    double n,ans=0.0;
+    scanf("%lf",&n);
+    while(n--){
+        ans+=6.0;
+        ans=1.0/ans;
+    }
+    ans+=3.0;
+    printf("%.10lf\n",ans);
+    
+    return 0;
+}
