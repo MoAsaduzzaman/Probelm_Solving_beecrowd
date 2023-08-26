3e95ef2e169d66878781b6516a38b9567ecaa584
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-26 12:49:56 +0600

    Bean

---
+++ 2791.c
@@ -0,0 +1,15 @@
+//Problem no-2791
+//https://www.beecrowd.com.br/judge/en/problems/view/2791
+
+#include<stdio.h>
+int main(){
+    int C1,C2,C3,C4;
+    scanf("%d %d %d %d",&C1,&C2,&C3,&C4);
+
+    if(C1==1) printf("1\n");
+    if(C2==1) printf("2\n");
+    if(C3==1) printf("3\n");
+    if(C4==1) printf("4\n");
+
+    return 0;
+}
