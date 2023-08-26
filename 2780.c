b20dadd20c8433b410cd827b86484aa103125f20
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-26 12:49:16 +0600

    Robot Basketball

---
+++ 2780.c
@@ -0,0 +1,20 @@
+//Problem no-2780
+//https://www.beecrowd.com.br/judge/en/problems/view/2780
+
+#include<stdio.h>
+int main(){
+    int D;
+    scanf("%d",&D);
+
+    if(D>=0 && D<=800){
+        printf("1\n");
+    }
+    if(D>800 && D<=1400){
+        printf("2\n");
+    }
+    if(D>1400 && D<=2000){
+        printf("3\n");
+    }
+   
+    return 0;
+}
