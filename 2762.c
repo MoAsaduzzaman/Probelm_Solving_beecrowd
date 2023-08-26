1a9b5a2415422177a41b4d12e1a0ed73d0850932
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-26 12:42:11 +0600

    Input and Output 6

---
+++ 2762.c
@@ -0,0 +1,12 @@
+//Problem no-2762
+//https://www.beecrowd.com.br/judge/en/problems/view/2762
+
+#include<stdio.h>
+int main(){
+    int x,y;
+    while(scanf("%d.%d",&x,&y)!=EOF){
+        printf("%d.%d\n",y,x);
+    }
+    
+    return 0;
+}
