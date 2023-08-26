e12b12f8b2767e6175f6f4f14fb02fbacf8de357
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-26 12:38:12 +0600

    Hot Dogs

---
+++ 2234.c
@@ -0,0 +1,13 @@
+//Problem no-2234
+//https://www.beecrowd.com.br/judge/en/problems/view/2134
+
+#include<stdio.h>
+int main(){
+    double H,P,AVG;
+    scanf("%lf %lf",&H,&P);
+
+    AVG=H/P;
+    printf("%.2lf\n",AVG);
+    
+    return 0;
+}
