0bb38d2c4103d959e03cb23404770e179382c39b
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-26 12:50:42 +0600

    The Output

---
+++ 2861.c
@@ -0,0 +1,16 @@
+//Problem no-2861
+//https://www.beecrowd.com.br/judge/en/problems/view/2861
+
+#include<stdio.h>
+int main(){
+    int N,i;
+    char a[100];
+
+    scanf("%d",&N);
+    for(i=0; i<N; i++){
+        gets(a);
+        printf("gzuz\n");
+    }
+    
+    return 0;
+}
