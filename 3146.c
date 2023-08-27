d33395e9783485dca75f40372f35964807233e35
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-27 09:49:38 +0600

    Riddles in the Dark

---
+++ 3146.c
@@ -0,0 +1,14 @@
+//Problem no-3146
+//https://www.beecrowd.com.br/judge/en/problems/view/3146
+
+#include<stdio.h>
+int main(){
+    double R,total_size,pi=3.14;
+    scanf("%lf",&R);
+
+    total_size=(2*pi*R);
+
+    printf("%.2lf\n",total_size);
+
+    return 0;
+}
