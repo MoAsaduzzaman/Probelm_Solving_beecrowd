414fe9c98f78ebfe4c4d043b226b51d7076325e2
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-27 10:33:11 +0600

    Dominó

---
+++ 3046.c
@@ -0,0 +1,14 @@
+//Problem no-3046
+//https://www.beecrowd.com.br/judge/en/problems/view/3046
+
+#include<stdio.h>
+int main(){
+    int N,ans;
+    scanf("%d",&N);
+
+    ans=(((N+1)*(N+2))/2);
+
+    printf("%d\n",ans);
+
+    return 0;
+}
