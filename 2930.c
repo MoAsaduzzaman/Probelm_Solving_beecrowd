75e88ad400c8ed715e36f463f8a3c549324c2eb3
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-27 11:52:46 +0600

    inal Thesis of Christmas Depression

---
+++ 2930.c
@@ -0,0 +1,21 @@
+//Problem no-2930
+//https://www.beecrowd.com.br/judge/en/problems/view/2830
+
+#include<stdio.h>
+int main(){
+    int E,D;
+    scanf("%d %d",&E,&D);
+
+    if(E>D){
+        printf("Eu odeio a professora!\n");
+    }else if((D-E)>=3){
+        printf("Muito bem! Apresenta antes do Natal!\n");
+    }else if((E+2)<24){
+        printf("Parece o trabalho do meu filho!\nTCC Apresentado!\n");
+    }else{
+        printf("Parece o trabalho do meu filho!\nFail! Entao eh nataaaaal!\n");
+    }
+
+    return 0;
+
+}
