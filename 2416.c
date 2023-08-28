9beee1b44340ef85e31bdfb0649603793232634d
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-29 00:05:43 +0600

    Corrida

---
+++ 2416.c
@@ -0,0 +1,13 @@
+//Problem no-2416
+//https://www.beecrowd.com.br/judge/en/problems/view/2416
+
+#include<stdio.h>
+int main(){
+    int a,b,div;
+    scanf("%d %d",&a,&b);
+    div=a%b;
+
+    printf("%d\n",div);
+    
+    return 0;
+}
