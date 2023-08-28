9150b125b9d9758958e8108e4db66c10e698f7e6
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-29 00:03:22 +0600

    Triangles and Regular Polygons

---
+++ 2058.c
@@ -0,0 +1,14 @@
+//Problem no-2058
+//https://www.beecrowd.com.br/judge/en/problems/view/2058
+
+#include<stdio.h>
+int main(){
+    int N,ans;
+    scanf("%d",&N);
+
+    ans=N-2;
+
+    printf("%d\n",ans);
+
+    return 0;
+}
