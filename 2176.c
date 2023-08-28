34b9d9d30d6322ac2c3232cef7e5e13ea83cfbe9
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-28 12:40:43 +0600

    Parity

---
+++ 2176.c
@@ -0,0 +1,21 @@
+//Problem no-2176
+//https://www.beecrowd.com.br/judge/en/problems/view/2176
+
+#include<stdio.h>
+#include <string.h>
+int main(){
+    char str[150];
+    int i, cnt=0;
+    scanf("%s", str);
+    for(i=0; str[i]; i++)
+        if(str[i]=='1')
+            cnt++;
+    if(cnt%2 == 0)
+        str[i] = '0';
+    else
+        str[i] = '1';
+    str[i+1] = '\0';
+    printf("%s\n", str);
+    return 0;
+} 
+
