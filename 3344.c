c502933e433e0f507cf691c12ed550337d562ad8
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-30 00:02:46 +0600

    Brute

---
+++ 3344.c
@@ -0,0 +1,12 @@
+//Problem no-3344
+//https://www.beecrowd.com.br/judge/en/problems/view/3344
+
+#include<stdio.h>
+int main(){
+    int x;
+
+    scanf("%d",&x);
+    printf("4\n");
+
+    return 0;
+}
