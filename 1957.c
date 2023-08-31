beeb1e90a86ddb707a7b563d259e7bf67b7e5906
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-31 13:02:06 +0600

    Converting to Hexadecimal

---
+++ 1957.c
@@ -0,0 +1,11 @@
+//Problem no-1957
+//https://www.beecrowd.com.br/judge/en/problems/view/1957
+
+#include<stdio.h>
+int main(){
+    long int a;
+    while(scanf("%ld",&a)!=EOF){
+        printf("%X\n",a);
+    }
+    return 0;
+}
